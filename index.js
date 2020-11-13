b79de3852cb614606c3d192c2b1100a694ac9717
MaciejSpalek <spwrtt@gmail.com>
2020-11-13 18:21:17 +0100

    Connection with datebase

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
 const authRoute = require('./Routes/auth');
+
+
+mongoose.connect('mongodb+srv://spwrtt:<password>@cluster0.zver3.mongodb.net/<dbname>?retryWrites=true&w=majority', 
+{ useNewUrlParser: true },
+() => console.log("Contected to db"))
+
 
 // Route Middlewares
 app.use('/api/user', authRoute)
-
-
 app.listen(5000, () => console.log(`Server is running...`));
