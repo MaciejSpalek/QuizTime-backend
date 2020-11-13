a9bbf99a35cf889b9ea4a9d716fedb2c9da4e225
MaciejSpalek <spwrtt@gmail.com>
2020-11-13 17:34:39 +0100

    Add auth.js route to Routes directory

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
+const authRoute = require('./Routes/auth');
+
+// Route Middlewares
+app.use('/api/user', authRoute)
+
+
 app.listen(5000, () => console.log(`Server is running...`));
