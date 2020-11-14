d8fb480b35cb313d68e19c2db5deca339f317ed0
MaciejSpalek <spwrtt@gmail.com>
2020-11-14 12:12:56 +0100

    Create register request and validation

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,7 +1,33 @@
 const router = require('express').Router();
+const User = require('../model/User');
+const { registerValidation } = require('../validation');
 
-router.post('/register', (req, res) => {
-    res.send('Register');
+
+
+router.post('/register', async (req, res) => {
+    
+    //Validation the data before we a user
+    const {error} = registerValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    //Checking if the user is already in the databease
+    const emailExist = await User.findOne({email: req.body.email});
+    if(emailExist) return res.status(400).send('Email already exists')
+
+
+    // Create a new user
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
+
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser)
+    } catch(error) { 
+        res.status(400).send(error);
+    }
 })
 
 module.exports = router;
--- index.js
+++ index.js
@@ -7,10 +7,13 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true },
-() => console.log("Contected to db"))
+mongoose.connect(
+    process.env.DB_CONNECT, 
+    { useNewUrlParser: true }, () => 
+        console.log("Contected to db"
+))
 
 
-// Route Middlewares
-app.use('/api/user', authRoute)
+app.use(express.json()); //Middleware
+app.use('/api/user', authRoute) // Route Middlewares
 app.listen(5000, () => console.log(`Server is running...`));
