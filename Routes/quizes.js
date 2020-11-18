e47413a4923b160330e6f35b4ecf258adc20dd6e
MaciejSpalek <spwrtt@gmail.com>
2020-11-18 16:02:13 +0100

    Add quizes middleware and route

---
+++ Routes/quizes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+router.post('/addQuiz', (req, res) => {
+    
+})
+
+module.exports = router;
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/user');
+const quizesRoute = require('./routes/quizes');
 
 dotenv.config();
 
@@ -21,5 +22,6 @@ app.use(express.json()); //Middleware
 
 app.use('/api/auth', authRoute); // Route Middlewares
 app.use('/api/user', userRoute); // Route Middlewares
+app.use('/api/quizes', quizesRoute); // Route Middlewares
 
 app.listen(PORT, () => console.log(`Server is running...`));
