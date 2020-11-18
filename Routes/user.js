6b7dcc631365dff1fa022db854a277239bdc5bfb
MaciejSpalek <spwrtt@gmail.com>
2020-11-18 15:49:58 +0100

    Add .env and node_modules in gitignore file

--- Routes/user.js
+++ Routes/user.js
@@ -1,18 +1,12 @@
 const router = require("express").Router();
 const User = require("../model/User");
 
-// router.post("/name", async (req, res) => {
-//   // const { name } = req.query;
-//   const userExist = User.findOne({name: req.body.name});
-//   res.send(userExist);
-// });
-
 router.get("/allUsers", async (req, res) => {
   const users = await User.find({});
   res.json(users);
 });
 
-router.get("/", async (req, res) => {
+router.get("/singleUser", async (req, res) => {
   const { name } = req.query;
   const userExist = await User.findOne({name});
   res.json(userExist);
