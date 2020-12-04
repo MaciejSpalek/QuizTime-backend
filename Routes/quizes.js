443be53fdc196c9b9806541453357677a706289c
MaciejSpalek <spwrtt@gmail.com>
2020-12-04 13:43:54 +0100

    Refactor singleQuiz route

--- Routes/quizes.js
+++ Routes/quizes.js
@@ -2,46 +2,42 @@ const router = require('express').Router();
 const Quiz = require('../model/Quiz');
 
 router.post('/addQuiz', async (req, res) => {
-    const quiz = new Quiz({
-        title: req.body.title,
-        author: req.body.author,
-        amountOfQuestions: req.body.amountOfQuestions,
-        colors: req.body.colors,
-        iconName: req.body.iconName,
-        questions: req.body.questions
-    });
+  const quiz = new Quiz({
+    title: req.body.title,
+    author: req.body.author,
+    amountOfQuestions: req.body.amountOfQuestions,
+    colors: req.body.colors,
+    iconName: req.body.iconName,
+    questions: req.body.questions,
+  });
 
-    try {
-        const savedQuiz = await quiz.save();
-        res.send(savedQuiz)
-    } catch(error) { 
-        res.send(error)
-    }
+  try {
+    const savedQuiz = await quiz.save();
+    res.send(savedQuiz);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 router.get('/userQuizzes', async (req, res) => {
-    const { author } = req.query;
-    const quiz = await Quiz.find({author});
-    res.json(quiz);
+  const { author } = req.query;
+  const quiz = await Quiz.find({ author });
+  res.json(quiz);
 });
 
-
 router.get('/allQuizzes', async (req, res) => {
-    const quizzes = await Quiz.find({});
-    res.json(quizzes);
+  const quizzes = await Quiz.find({});
+  res.json(quizzes);
 });
 
 router.get('/singleQuiz', async (req, res) => {
-    const { id } = req.query;
-    try {
-        const quiz = await Quiz.findById(id);
-        res.json(quiz);
-    } catch(error) { 
-        res.status(400).json({
-            message: "Wrong quiz id"
-        });
-    }
+  const { id, author } = req.query;
+  try {
+    const quiz = await Quiz.findOne({'_id': id, 'author': author});
+    quiz ? res.json(quiz) : res.json({ message: 'Quiz not found' });
+  } catch (error) {
+    res.status(400).json({ message: 'Quiz not found' });
+  }
 });
-
 
 module.exports = router;
